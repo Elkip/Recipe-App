14e1ba33711951aa688cdbaf56b86ed2811fbda1
elkip <mitchiscool51@gmail.com>
2020-10-11 10:50:30 -0400

    Added test for UoM repository

---
+++ src/test/java/springframework/guru/recipe/repositories/UnitOfMeasureRepositoryTest.java
@@ -0,0 +1,31 @@
+package springframework.guru.recipe.repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import springframework.guru.recipe.domain.UnitOfMeasure;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryTest {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByDescription() {
+        Optional<UnitOfMeasure> optionalUnitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", optionalUnitOfMeasure.get().getDescription());
+    }
+}
