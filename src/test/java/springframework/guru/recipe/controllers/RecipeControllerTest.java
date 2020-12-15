1c78fe89dbeb37517f71c24a4cdc1e3b807c27b0
elkip <mitchiscool51@gmail.com>
2020-12-14 19:30:27 -0500

    Added customized error messages to recipeform

--- src/test/java/springframework/guru/recipe/controllers/RecipeControllerTest.java
+++ src/test/java/springframework/guru/recipe/controllers/RecipeControllerTest.java
@@ -104,6 +104,23 @@ class RecipeControllerTest {
     }
 
     @Test
+    public void testPostNewRecipeFormValidationFail() throws Exception {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(2L);
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+        mockMvc.perform(post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+                .param("cookTime", "3000")
+        )
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/recipeform"))
+                .andExpect(model().attributeExists("recipe"));
+    }
+
+    @Test
     public void testGetUpdateView() throws Exception {
         RecipeCommand command = new RecipeCommand();
         command.setId(2L);
